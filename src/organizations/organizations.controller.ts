cf641fcc015eb1797cd68db2849aab12c62d9397
Jose Diago <owlicodes@gmail.com>
2024-11-28 13:52:50 +0800

    Create organization api route.

--- src/organizations/organizations.controller.ts
+++ src/organizations/organizations.controller.ts
@@ -1,19 +1,32 @@
-import { Controller, Get, Request, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from "@nestjs/common";
 
 import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
 import { TUser } from "../types";
+import { CreateOrganizationDto } from "./dtos/create-organization.dto";
 import { OrganizationsService } from "./organizations.service";
 
 @Controller({
   path: "organizations",
   version: "1",
 })
+@UseGuards(JwtAuthGuard)
 export class OrganizationsController {
   constructor(private readonly organizationsService: OrganizationsService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Get()
   findOrganizations(@Request() req: { user: TUser }) {
     return this.organizationsService.findOrganizations(req.user.id);
+  }
+
+  @Post()
+  createOrganization(@Body() data: CreateOrganizationDto) {
+    return this.organizationsService.createOrganization(data);
   }
 }
