12ca5a8720604bdcc6f2fc816c3e96a3deb0ef17
Jose Diago <owlicodes@gmail.com>
2024-12-03 09:18:55 +0800

    Changing the validation for startDate and endDate of CreateSprintDto.

--- src/sprints/dtos/create-sprint.dto.ts
+++ src/sprints/dtos/create-sprint.dto.ts
@@ -1,16 +1,14 @@
 import { Type } from "class-transformer";
-import { IsDate, IsEnum, IsISO8601, IsString } from "class-validator";
+import { IsDate, IsEnum, IsString } from "class-validator";
 
 export class CreateSprintDto {
   @IsString()
   name: string;
 
-  @IsISO8601()
   @IsDate()
   @Type(() => Date)
   startDate: Date;
 
-  @IsISO8601()
   @IsDate()
   @Type(() => Date)
   endDate: Date;
